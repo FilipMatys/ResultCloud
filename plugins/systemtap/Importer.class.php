679651f652f8d9b268d57a0fc57ba3b02dc3f37c
Filip Matys <matysfilip@gmail.com>
2014-04-04 22:49:01 +0200

    SYSTEMTAP parser

---
+++ plugins/systemtap/Importer.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Importer short summary.
+ *
+ * Importer description.
+ *
+ * @version 1.0
+ * @author Filip
+ */
+class Importer
+{
+    /**
+     * Import given file based on metadata
+     * @param metadata - meta information about given data
+     * @param FileParser file - uploaded file
+     * @return TestSuite - object containing one test suite result
+     */
+    public static function Import($validation, $file)  {
+   
+        return Parser::ParseImport($file->getTemp());
+    }
+}
---
+++ plugins/systemtap/Parser.class.php
@@ -0,0 +1,64 @@
+<?php
+include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Library.utility.php');
+
+// Get libraries
+Library::using(Library::UTILITIES);
+Library::using(Library::CORLY_ENTITIES);
+
+/**
+ * Parser short summary.
+ *
+ * Parser description.
+ *
+ * @version 1.0
+ * @author Filip
+ */
+class Parser
+{
+    /**
+     * Parse uploaded data for import
+     */
+    public static function ParseImport($data)    {
+        // Load uploaded file as string
+        $fileRows = file($data);
+        
+        $Category = new Category("Default");
+        // Iterate through array (rows)
+        foreach ($fileRows as $row) {
+            
+            // Check for date and time
+            if (!isset($Submission) && preg_match("/Test Run By (.*) on (.*)/", $row, $headerMatch))    {
+                $Submission = new Submission($headerMatch[2]);
+            }
+        
+            // Check for test case header
+            if (preg_match("/Running (\.\.\/.*) \.{3}/", $row, $testCaseMatches))   {
+                
+                // If test case was set, add it to category,
+                // because we are creating new test case
+                if (isset($TestCase))   {
+                    $Category->AddTestCase($TestCase);
+                }
+                
+                // Create new test case
+                $TestCase = new TestCase($testCaseMatches[1]);
+            }
+            // Check for result
+            else if (preg_match("/([A-Z]*): (.*)/", $row, $resultMatches))  {
+                // Create result object
+                $Result = new Result($resultMatches[2], $resultMatches[1]);
+                
+                // Add result to test case, if there is any
+                if (isset($TestCase))   {
+                    $TestCase->AddResult($Result);
+                }
+            }
+        }
+        
+        // Add category to submission
+        $Submission->AddCategory($Category);
+        
+        $validation = new ValidationResult($Submission);
+        return $validation;
+    }   
+}
