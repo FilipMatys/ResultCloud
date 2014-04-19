18ad8d706033396d250a939a31a17c44d2398f30
Filip Matys <matysfilip@gmail.com>
2014-04-20 01:25:30 +0200

    GLIBC submission overview

--- plugins/glibc/Visualization.class.php
+++ plugins/glibc/Visualization.class.php
@@ -5,6 +5,7 @@ include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'
 Library::using(Library::UTILITIES);
 Library::using(Library::CORLY_ENTITIES);
 Library::using(Library::VISUALIZATION_PROJECT);
+Library::using(Library::VISUALIZATION_SUBMISSION);
 Library::using(Library::VISUALIZATION_COMPONENT_GOOGLECHART);
 Library::using(Library::PLUGINS .DIRECTORY_SEPARATOR. 'glibc' .DIRECTORY_SEPARATOR. 'visualization');
 
@@ -27,7 +28,12 @@ class Visualization
         return new ValidationResult(ProjectVisualization::Visualize($project));
     }
     
+    /**
+     * Visualize submission
+     * @param mixed $submission 
+     * @return mixed
+     */
     public static function VisualizeSubmission($submission)    {
-    
+        return new ValidationResult(SubmissionVisualization::Visualize($submission));
     }
 }
---
+++ plugins/glibc/visualization/SubmissionVirtualization.class.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * SubmissionVisualization short summary.
+ *
+ * SubmissionVisualization description.
+ *
+ * @version 1.0
+ * @author Filip
+ */
+class SubmissionVisualization
+{
+    /**
+     * Create submission object for visualization
+     * @param SubmissionTSE $submission 
+     * @return mixed
+     */
+    public static function Visualize(SubmissionTSE $submission) {
+        // Initialize submission visualization
+        $submissionVisualization = new SubmissionOverviewVisualization();
+        
+        // Set submission overview list
+        $submissionVisualization->SetSubmissionOverviewList(SubmissionVisualization::GetSubmissionOverviewList($submission));
+        
+        // Set submission overview chart
+        $submissionVisualization->SetSubmissionOverviewChart(SubmissionVisualization::GetSubmissionOverviewChart($submission));
+        
+        // return result
+        return $submissionVisualization->ExportObject();
+    }
+    
+    /**
+     * Get submission overview list data for visualization
+     * @param SubmissionTSE $project 
+     * @return mixed
+     */
+    private static function GetSubmissionOverviewList(SubmissionTSE $submission)    {
+        // Initialize list
+        $submissionOverviewList = new SubmissionOverviewList();
+        
+        foreach ($submission->GetCategories() as $category)
+        {
+            // Create new list item
+            $submissionOverviewListItem = new SubmissionOverviewListItem($category);
+            // Add item to list
+            $submissionOverviewList->AddItem($submissionOverviewListItem);
+        }
+        
+        // Set available views
+        $submissionOverviewList->AddView(SubmissionOverviewType::GroupedView);
+        $submissionOverviewList->AddView(SubmissionOverviewType::ListView);
+        
+        // Return result
+        return $submissionOverviewList;
+    }
+    
+    /**
+     * Get Submission Overview Chart for visualization
+     * @param SubmissionTSE $submission 
+     * @return mixed
+     */
+    private static function GetSubmissionOverviewChart(SubmissionTSE $submission)    {
+        // Initialize Google chart object
+        $submissionOverviewChart = new SubmissionOverviewChart();
+        $googleChart = new GoogleChart();
+        $googleChart->setType(GCType::PIE_CHART);
+        
+        // Assign options to chart
+        $googleChart->setOptions(SubmissionVisualization::GetGCOptions());
+        
+        // Get data
+        $googleChart->setData(SubmissionVisualization::GetGCData($submission));
+        
+        // Set chart as chart overview
+        $submissionOverviewChart->SetChart($googleChart);
+        
+        // return google chart object
+        return $submissionOverviewChart;
+    }
+    
+    /**
+     * Get data for submission google chart
+     * @param SubmissionTSE $submission 
+     * @return mixed
+     */
+    private static function GetGCData(SubmissionTSE $submission) {
+        // Initialize data
+        $gcData = new GCData();
+        
+        // Initialize results list
+        $subResults = array();
+        
+        // First, we need to get all results of given submission
+        foreach ($submission->GetCategories() as $category) {
+            // Iterate through each test case
+            foreach ($category->GetTestCases() as $testCase)    {
+                // Iterate through each result and get "status" value
+                foreach ($testCase->GetResults() as $result) {
+                    $subResults[] = $result->GetValue();
+                }
+            }
+        }
+        
+        // Create STATUS column
+        $gcStatusCol = new GCCol();
+        $gcStatusCol->setId("error");
+        $gcStatusCol->setLabel("Error");
+        $gcStatusCol->setType("string");
+        // Add column to data set
+        $gcData->AddColumn($gcStatusCol);
+        
+        // Create VALUE column
+        $gcValueCol = new GCCol();
+        $gcValueCol->setId("value");
+        $gcValueCol->setLabel("Value");
+        $gcValueCol->setType("number");
+        // Add column to data set
+        $gcData->AddColumn($gcValueCol);
+        
+        // Then, get unique array of column values
+        $columns = array_unique($subResults);
+        
+        // Create rows with cells
+        $lSubResults = new LINQ($subResults);
+        foreach ($columns as $column) {
+            // Create new row
+            $gcRow = new GCRow();
+            
+            // Create label cell
+            $gcLabelCell = new GCCell();
+            $gcLabelCell->setValue($column);
+            // Add cell to row
+            $gcRow->AddCell($gcLabelCell);
+            
+            // Create value cell
+            $gcValueCell = new GCCell();
+            $gcValueCell->setValue($lSubResults->Where(null, LINQ::IS_EQUAL, $column)->Count());
+            // Add cell to row
+            $gcRow->AddCell($gcValueCell);
+            
+            // Add row to data
+            $gcData->AddRow($gcRow);
+        }
+        
+        // Return result
+        return $gcData;
+    }
+    
+    /**
+     * Get google chart options
+     * @return mixed
+     */
+    private static function GetGCOptions()  {
+        // Create options and set all in it
+        $gcOptions = new GCOptions();
+        $gcOptions->setTitle("Errors overview");
+        
+        // Return options
+        return $gcOptions;
+    }
+}
