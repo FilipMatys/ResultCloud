c7b0e120b22afa364852eab33c1c1c995402acca
Filip Matys <matysfilip@gmail.com>
2014-04-21 20:56:46 +0200

    Several GUI extensions

--- visualization/components/submissionList/js/SubmissionList.js
+++ visualization/components/submissionList/js/SubmissionList.js
@@ -5,8 +5,31 @@
         scope:  {
             Data: '=data'
         },
-        controller: function ($scope) {
+        controller: function ($scope, $state, $stateParams) {
 
+            // Compare all selected submissions
+            $scope.Differ = function () {
+                // Get all submissions market for difference
+                var submissions = [];
+                angular.forEach($scope.Data.Submissions, function (item) {
+                    if (item.Submission.Differ)
+                        submissions.push(item.Submission.Id);
+                });
+
+                // Check if there is anything to compare
+                if (submissions.length < 2)
+                    return;
+
+                // Go to differ page
+                $state.go('home.difference-overview', { projectId: $stateParams.projectId, differenceArray: submissions.join("&") });
+            }
+
+            // Watch for "select all" change
+            $scope.$watch('SelectAll', function (value) {
+                angular.forEach($scope.Data.Submissions, function (item) {
+                    item.Submission.Differ = value;
+                });
+            });
         }
     }
 });
